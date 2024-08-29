c4ee17092715baa7cf4292e7057d0976307f4e45
abxh <83485102+abxh@users.noreply.github.com>
2024-08-29 23:31:02 +0200

    minor change

--- asciirast/draw.h
+++ asciirast/draw.h
@@ -6,4 +6,4 @@
 #include "color_encoding.h"
 
 void draw_point(struct canvas_type* canvas, const uint32_t x, const uint32_t y, const struct rgb_type fg_color,
-                struct rgb_type bg_color, const char ascii_char, const uint32_t z_order);
+                const struct rgb_type bg_color, const char ascii_char, const uint32_t z_order);
