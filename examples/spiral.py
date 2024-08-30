65b73013c04d2546cb58e0b3e9b2fe68d16729c4
abxh <83485102+abxh@users.noreply.github.com>
2024-08-31 00:37:15 +0200

    add donut

--- examples/spiral.py
+++ examples/spiral.py
@@ -19,8 +19,8 @@ def rotate_2d(xy: Tuple[float, float], radians: float) -> Tuple[float, float]:
 
 def get_pixel_coords(xy: Tuple[float, float], canvas: Canvas) -> Tuple[int, int]:
     x, y = xy
-    px, py = (x + 1.0) / 2.0 * canvas.w, (y + 1.0) / 2.0 * canvas.h
-    return int(px), canvas.h - 1 - int(py)
+    px, py = (x + 1.0) / 2.0 * canvas.width, (y + 1.0) / 2.0 * canvas.height
+    return int(px), canvas.height - 1 - int(py)
 
 
 def interpolate_float(x: float, y: float, t: float) -> float:
@@ -40,22 +40,30 @@ def main() -> None:
     ascii_palette = "@%#*+=-:. "
     color_red = RGBColor(255, 0, 0)
     color_yellow = RGBColor(255, 255, 0)
-
     curr_angle_deg = 0
+
+    def render():
+        nonlocal curr_angle_deg
+        for o in range(6):
+            for i in range(7):
+                angle_rad = math.radians(curr_angle_deg - 20 * i - 60 * o)
+                vec = rotate_2d((0.6 - 0.1 * i, 0.6 - 0.1 * i), angle_rad)
+                color = interpolate_color(color_red, color_yellow, i / 6)
+                draw_point(canvas, *get_pixel_coords(vec, canvas), ascii_palette[i], color)
+        curr_angle_deg -= 10
+
     with printutils.hidden_cursor():
         while True:
-            for o in range(6):
-                for i in range(7):
-                    angle_rad = math.radians(curr_angle_deg - 20 * i - 60 * o)
-                    vec = rotate_2d((0.6 - 0.1 * i, 0.6 - 0.1 * i), angle_rad)
-                    color = interpolate_color(color_red, color_yellow, i / 6)
-                    draw_point(canvas, *get_pixel_coords(vec, canvas), ascii_palette[i], color)
-            curr_angle_deg -= 10
-            sleep(1 / 10)
-
+            render()
             canvas.print_formatted()
             canvas.clear()
-            printutils.move_up_lines(canvas.h)
+            printutils.move_up_lines(canvas.height)
+
+            sleep(1 / 10)
+
+    canvas.clear()
+    render()
+    canvas.print_formatted()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -7,6 +7,27 @@ if is_windows := os.name == "nt":
 
     just_fix_windows_console()
 
-from examples import spiral
 
-spiral.main()
+def iter_submodules(module):
+    import pkgutil
+    import importlib
+
+    for _, module_name, _ in pkgutil.walk_packages(
+        module.__path__, module.__name__ + "."
+    ):
+        yield importlib.import_module(module_name)
+
+
+import examples
+
+l = list(iter_submodules(examples))
+
+for i, module in enumerate(l):
+    print(f"[{i}]: {module.__name__.split('.')[1]}")
+
+if not (inp := input()).isdigit():
+    print(f"{inp} is not a integer.")
+elif not 0 <= int(inp) < len(l):
+    print(f"{inp} is not a valid index!")
+else:
+    l[int(inp)].main()
