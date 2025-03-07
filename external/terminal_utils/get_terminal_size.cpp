15a0d2e0f6c23abd64c021433853eb87a6915d1e
abxh <83485102+abxh@users.noreply.github.com>
2025-03-07 16:57:27 +0100

    get_terminal_size: minor improvement

--- external/terminal_utils/get_terminal_size.cpp
+++ external/terminal_utils/get_terminal_size.cpp
@@ -10,14 +10,16 @@
 #define VC_EXTRALEAN
 #include <Windows.h>
 #elif defined(__linux__) || defined(__APPLE__)
-#include <sys/ioctl.h>
 #include <cstdio>
+#include <sys/ioctl.h>
 #endif
 
 #include "terminal_utils.h"
 
-void get_terminal_size(int& out_width, int& out_height) {
-    int width, height;
+void
+get_terminal_size(int& out_width, int& out_height)
+{
+    out_width = out_height = 0;
 #if defined(_WIN32)
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
@@ -28,5 +30,5 @@ void get_terminal_size(int& out_width, int& out_height) {
     ioctl(fileno(stdout), TIOCGWINSZ, &w);
     out_width = (int)(w.ws_col);
     out_height = (int)(w.ws_row);
-#endif  // Windows/Linux
+#endif // Windows/Linux
 }
