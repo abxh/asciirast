d039ca8c8d5046e1811185ae06103eca757631b9
abxh <83485102+abxh@users.noreply.github.com>
2025-03-12 00:17:15 +0100

    rearrage util functions

--- asciirast/math/types/Vec.h
+++ asciirast/math/types/Vec.h
@@ -9,118 +9,13 @@
 #include <cassert>
 #include <cmath>
 #include <cstdlib>
-#include <limits>
 #include <ostream>
 #include <type_traits>
 
+#include "../utils.h"
 #include "VecBase.h"
 
 namespace asciirast::math {
-
-/**
- * @brief Check if equal to floating type value, given a precision for ulps
- * (units in last place).
- *
- * The lower, the more precise --- desirable for small floats.
- * The higher, the less precise --- desirable for large floats
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_equal(const T& x, const T& y, const unsigned ulps_)
-{
-    // Based on:
-    // https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
-
-    const T ulps = static_cast<T>(ulps_);
-    const T min = std::min(std::fabs(x), std::fabs(y));
-    const T exp = min < std::numeric_limits<T>::min() ? std::numeric_limits<T>::min_exponent - 1 : std::ilogb(min);
-
-    return std::fabs(x - y) <= ulps * std::ldexp(std::numeric_limits<T>::epsilon(), exp);
-}
-
-/**
- * @brief Check if almost equal to another floating value with default
- * precision.
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_equal(const T& x, const T& y)
-    requires(std::is_same_v<T, float>)
-{
-    // default precision based on:
-    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
-
-    return almost_equal(x, y, 9);
-}
-
-/**
- * @brief Check if almost equal to another floating value with default
- * precision.
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_equal(const T& x, const T& y)
-    requires(std::is_same_v<T, double>)
-{
-    // default precision based on:
-    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format
-
-    return almost_equal(x, y, 17);
-}
-
-/**
- * @brief Check if less than floating type value, given a precision for ulps
- * (units in last place).
- *
- * The lower, the more precise --- desirable for small floats.
- * The higher, the less precise --- desirable for large floats
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_less_than(const T& x, const T& y, const unsigned ulps_)
-{
-    const T ulps = static_cast<T>(ulps_);
-    const T min = std::min(std::fabs(x), std::fabs(y));
-    const T exp = min < std::numeric_limits<T>::min() ? std::numeric_limits<T>::min_exponent - 1 : std::ilogb(min);
-
-    return x - y < -ulps * std::ldexp(std::numeric_limits<T>::epsilon(), exp);
-}
-
-/**
- * @brief Check if almost less than another floating value with default
- * precision.
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_less_than(const T& x, const T& y)
-    requires(std::is_same_v<T, float>)
-{
-    // default precision based on:
-    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
-
-    return almost_less_than(x, y, 9);
-}
-
-/**
- * @brief Check if almost less than another floating value with default
- * precision.
- */
-template<typename T>
-    requires(std::is_floating_point_v<T>)
-bool
-almost_less_than(const T& x, const T& y)
-    requires(std::is_same_v<T, double>)
-{
-    // default precision based on:
-    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format
-
-    return almost_less_than(x, y, 17);
-}
 
 /**
  * @brief Trait to check if vector can constructed from arguments.
--- asciirast/math/utils.h
+++ asciirast/math/utils.h
@@ -27,4 +27,109 @@ angle_as_degrees(const T radians)
     return T{ 360 } * radians / PI_2<T>;
 }
 
+/**
+ * @brief Check if equal to floating type value, given a precision for ulps
+ * (units in last place).
+ *
+ * The lower, the more precise --- desirable for small floats.
+ * The higher, the less precise --- desirable for large floats
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_equal(const T& x, const T& y, const unsigned ulps_)
+{
+    // Based on:
+    // https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+
+    const T ulps = static_cast<T>(ulps_);
+    const T min = std::min(std::fabs(x), std::fabs(y));
+    const T exp = min < std::numeric_limits<T>::min() ? std::numeric_limits<T>::min_exponent - 1 : std::ilogb(min);
+
+    return std::fabs(x - y) <= ulps * std::ldexp(std::numeric_limits<T>::epsilon(), exp);
+}
+
+/**
+ * @brief Check if almost equal to another floating value with default
+ * precision.
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_equal(const T& x, const T& y)
+    requires(std::is_same_v<T, float>)
+{
+    // default precision based on:
+    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+
+    return almost_equal(x, y, 9);
+}
+
+/**
+ * @brief Check if almost equal to another floating value with default
+ * precision.
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_equal(const T& x, const T& y)
+    requires(std::is_same_v<T, double>)
+{
+    // default precision based on:
+    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+
+    return almost_equal(x, y, 17);
+}
+
+/**
+ * @brief Check if less than floating type value, given a precision for ulps
+ * (units in last place).
+ *
+ * The lower, the more precise --- desirable for small floats.
+ * The higher, the less precise --- desirable for large floats
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_less_than(const T& x, const T& y, const unsigned ulps_)
+{
+    const T ulps = static_cast<T>(ulps_);
+    const T min = std::min(std::fabs(x), std::fabs(y));
+    const T exp = min < std::numeric_limits<T>::min() ? std::numeric_limits<T>::min_exponent - 1 : std::ilogb(min);
+
+    return x - y < -ulps * std::ldexp(std::numeric_limits<T>::epsilon(), exp);
+}
+
+/**
+ * @brief Check if almost less than another floating value with default
+ * precision.
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_less_than(const T& x, const T& y)
+    requires(std::is_same_v<T, float>)
+{
+    // default precision based on:
+    // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+
+    return almost_less_than(x, y, 9);
+}
+
+/**
+ * @brief Check if almost less than another floating value with default
+ * precision.
+ */
+template<typename T>
+    requires(std::is_floating_point_v<T>)
+bool
+almost_less_than(const T& x, const T& y)
+    requires(std::is_same_v<T, double>)
+{
+    // default precision based on:
+    // https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+
+    return almost_less_than(x, y, 17);
+}
+
 } // namespace asciirast::math
