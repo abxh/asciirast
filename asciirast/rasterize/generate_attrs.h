0368a1aee6efed053805acbfd4f577cee7b5a512
abxh <83485102+abxh@users.noreply.github.com>
2025-03-21 21:12:42 +0100

    corrections to interpolators

--- asciirast/rasterize/generate_attrs.h
+++ asciirast/rasterize/generate_attrs.h
@@ -10,15 +10,29 @@ namespace asciirast::rasterize {
 
 template<VaryingType Varying>
 static std::generator<Varying>
-generate_attrs(const math::F& len,
-               const math::F& len_inv,
-               const ProjectedFragment<Varying>& proj0,
-               const ProjectedFragment<Varying>& proj1)
+generate_attrs_simple(const math::F& len,
+                      const math::F& len_inv,
+                      const ProjectedFragment<Varying>& proj0,
+                      const ProjectedFragment<Varying>& proj1)
+{
+    Varying acc = proj0.attrs;
+    const Varying inc = len_inv * (proj1.attrs + -1 * proj0.attrs);
+
+    const auto len_i = static_cast<math::I>(len);
+    for (math::I i = 0; i < len_i; i++) {
+        co_yield acc;
+        acc = std::move(acc + inc);
+    }
+}
+
+template<VaryingType Varying>
+static std::generator<Varying>
+generate_attrs_perspective_corrected(const math::F& len,
+                                     const math::F& len_inv,
+                                     const ProjectedFragment<Varying>& proj0,
+                                     const ProjectedFragment<Varying>& proj1)
 {
     auto perspective_corrected = [&proj0, &proj1](const math::F t) {
-        if (!std::isfinite(proj0.depth)) {
-            return t;
-        }
         return t * proj0.depth / ((1 - t) * proj0.depth + t * proj1.depth);
     };
 
@@ -29,6 +43,20 @@ generate_attrs(const math::F& len,
     for (math::I i = 0; i < len_i; i++) {
         co_yield lerp(proj0.attrs, proj1.attrs, perspective_corrected(acc_t));
         acc_t += inc_t;
+    }
+}
+
+template<VaryingType Varying>
+static std::generator<Varying>
+generate_attrs(const math::F& len,
+               const math::F& len_inv,
+               const ProjectedFragment<Varying>& proj0,
+               const ProjectedFragment<Varying>& proj1)
+{
+    if (std::isfinite(proj0.depth) && std::isfinite(proj1.depth)) {
+        return generate_attrs_perspective_corrected(len, len_inv, proj0, proj1);
+    } else {
+        return generate_attrs_simple(len, len_inv, proj0, proj1);
     }
 }
 
--- asciirast/rasterize/generate_depth.h
+++ asciirast/rasterize/generate_depth.h
@@ -9,7 +9,7 @@ namespace asciirast::rasterize {
 static std::generator<math::F>
 generate_depth(const math::F& len, const math::F& len_inv, const math::F& depth0, const math::F& depth1)
 {
-    math::F acc = 0;
+    math::F acc = depth0;
     const math::F inc = (depth1 - depth0) * len_inv;
 
     const auto len_i = static_cast<math::I>(len);
