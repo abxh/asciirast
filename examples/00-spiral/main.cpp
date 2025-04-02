d35449f673a5b029e150f5886ed74232afae50d3
abxh <83485102+abxh@users.noreply.github.com>
2025-04-03 00:03:55 +0200

    minor: update 00-spiral

--- examples/00-spiral/main.cpp
+++ examples/00-spiral/main.cpp
@@ -213,7 +213,7 @@ main(void)
 
         t.render();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         t.clear_and_update_size();
 
