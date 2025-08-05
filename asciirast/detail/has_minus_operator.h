905702034a0c3528d960aaad705dbc760d75fa0d
abxh <83485102+abxh@users.noreply.github.com>
2025-08-05 16:42:42 +0000

    Update has_minus_operator.h

--- asciirast/detail/has_minus_operator.h
+++ asciirast/detail/has_minus_operator.h
@@ -2,7 +2,7 @@
  * @file has_minus_operator.h
  * @brief Has minus operator concept
  *
- * @todo Remove this, when the bug to have variadic concepts is fixed.
+ * @todo Remove this, when the bug to apply unnamed concept on variadics is fixed.
  *
  * See:
  * @link https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105644
