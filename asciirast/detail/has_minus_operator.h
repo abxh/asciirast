8ebf6ef3923fec155042cf8491fa9a606f284f51
abxh <83485102+abxh@users.noreply.github.com>
2025-08-05 18:41:57 +0200

    add link

--- asciirast/detail/has_minus_operator.h
+++ asciirast/detail/has_minus_operator.h
@@ -3,6 +3,9 @@
  * @brief Has minus operator concept
  *
  * @todo Remove this, when the bug to have variadic concepts is fixed.
+ *
+ * See:
+ * @link https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105644
  */
 
 #pragma once
